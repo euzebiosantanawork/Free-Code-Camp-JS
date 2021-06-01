b4b12dffb0ba5d48cd3db62dc1943080263cbe6e
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-01 15:40:29 -0300

    Create lesson55.js

---
+++ lesson55.js
@@ -0,0 +1,16 @@
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear  = "sweater";
+  console.log( outerWear );
+  // Only change code below this line
+
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+console.log(myOutfit());
+console.log( outerWear );
