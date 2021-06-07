fcbbd55a45bb09889ee52b4ba0c98d23690f83e5
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-07 14:31:06 -0300

    Create lesson57.js

---
+++ lesson57.js
@@ -0,0 +1,38 @@
+//try1
+// Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  num = num + 5;
+  return num;
+}
+
+console.log(addFive(3));
+// Only change code above this line
+
+addThree();
+addFive();
+
+//try2 // Setup
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+ 
+}
+
+
+addThree();
+addFive();
