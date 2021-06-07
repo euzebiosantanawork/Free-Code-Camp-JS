ca4da8efb53cae3e1db375bbc6b8a581b9fba193
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-07 14:37:01 -0300

    Create lesson58.js

---
+++ lesson58.js
@@ -0,0 +1,19 @@
+//try1
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+console.log(processArg (7));
+// Only change code below this line
+
+//try2
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
+// Only change code below this line
