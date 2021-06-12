282c0a602b47ac7398790d25e71467cf96f066ae
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-12 14:40:11 -0300

    Create lesson61.js

---
+++ lesson61.js
@@ -0,0 +1,17 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+
+  if(wasThatTrue){
+
+    return "Yes, that was true";
+
+  }
+
+  return "No, taht was false";
+
+  // Only change code above this line
+
+}
+
+trueOrFalse(true);
+trueOrFalse(false);
