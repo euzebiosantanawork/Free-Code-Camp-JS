dd73970b7d9706ad55804d8fc0579dca30f7270d
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-29 11:47:46 -0300

    Create lesson81.1.js

---
+++ lesson81.1.js
@@ -0,0 +1,23 @@
+function switchofStuff(val){
+  var answer = "";
+  switch(val) {
+      case"a":
+      answer = "apple";
+      break;
+      case"b":
+      answer = "bird";
+      break;
+      case "c":
+      answer = "cat";
+      break;
+      default:
+      answer = "stuff";
+      break;
+      }
+      return answer;
+      }
+      //Change this value to test
+      console.log(switchOfStuff("a")); 
+      console.log(switchOfStuff("b")); 
+      console.log(switchOfStuff("c")); 
+      console.log(switchOfStuff("d")); 
