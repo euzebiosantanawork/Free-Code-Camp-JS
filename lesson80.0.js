6c93171e67d409e6bfb5c4370d3022906c87ef5c
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-29 11:16:49 -0300

    Create lesson80.0.js

---
+++ lesson80.0.js
@@ -0,0 +1,35 @@
+/* example of switch statment 
+
+switch(lowercaseLetter) {
+  casa : "a"
+  console.log("A");
+  case: "b":
+  console.log("B");
+  break;
+}
+*/
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(answer) {
+    case "1":
+    console.log("alpha")
+    break;
+    case "2":
+    console.log("beta")
+    break;
+    case "3":
+    console.log("gamma")
+    break;
+    case "4":
+    console.log("delta")
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
