2bfc92c3d08bd5d928ad921302f687c76b25cf16
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-29 14:06:51 -0300

    Create lesson87.js

---
+++ lesson87.js
@@ -0,0 +1,8 @@
+var myDog = {
+// Only change code below this line
+"name" : "Milly",
+"legs" : 4,
+"tails": 1,
+"friends": [ "Luke" , "Santana" , "Topher" , "Denny"],
+// Only change code above this line
+};
