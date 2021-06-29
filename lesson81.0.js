989bce1532dbd486aa84524cb1787994a9f923f7
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-06-29 11:38:26 -0300

    Create lesson81.0.js

---
+++ lesson81.0.js
@@ -0,0 +1,44 @@
+/*
+A defalut statement should be the last case
+
+switch (num) {
+  case value 1:
+    statement1;
+    break;
+    case value2:
+    statement2;
+    break;
+    ...
+    default:
+    defaultStatement;
+    break;
+}
+*/
+
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch(val) {
+    case a:
+     apple;
+    break;
+    case b:
+     bird;
+    break;
+     case c:
+     cat;
+    break;
+    default:
+    defaultStatement;
+    break;
+    
+    
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
