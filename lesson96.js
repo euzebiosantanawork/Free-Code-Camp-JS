1c132d55c3978c720c4c41fe883a284123caf5ec
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 16:17:19 -0300

    Create lesson96.js

---
+++ lesson96.js
@@ -0,0 +1,21 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  } ,
+    
+  { 
+    "artist" : "Daft Punk",
+    "title":"Smoke on the water",
+    "release_year": 1996,
+    "formats": [ "CD","Casette", "LP"]
+    
+  }
+];
