d8d8f5970def4d44be496325330d8204aa78619d
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 15:22:58 -0300

    Create lesson92.example.js

---
+++ lesson92.example.js
@@ -0,0 +1,15 @@
+/*
+
+You can add new properties to existing JavaScript objects the same way you would modify them.
+
+Here how we would add a bark property to ourDog;
+
+*/
+
+ourDog.bark = "bow-wow";
+
+// or
+
+ourDog["bark"] = "bow-wow";
+
+/*Now when we evaluate ourDog.bark, we'll get his bark, bow-wow.*/
