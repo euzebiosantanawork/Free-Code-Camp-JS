d8eeaf74bb5714d6242b9e29c0a7680d49008bad
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 16:17:00 -0300

    Create lesson96.example.js

---
+++ lesson96.example.js
@@ -0,0 +1,26 @@
+/*
+
+Sometimes you may want to store data in a flexible Data Structure. 
+A JavaScript object is one way to handle flexible data. 
+They allow for arbitrary combinations of strings, 
+numbers, booleans, arrays, functions, and objects.
+
+Here's an example of a complex data structure:
+
+
+
+*/
+
+var ourMusic = [
+  { 
+    "artist" : "Daft Punk",
+    "title":"Homework",
+    "release_year": 1997,
+    "formats": [
+      "CD",
+      "Casette"
+      "LP""
+      ]'
+    "gold": true
+  }
+  ];
