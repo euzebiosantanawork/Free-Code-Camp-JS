4a9cc6ce8aa4a9a39a6017dec64b3a542e6522cb
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 16:39:36 -0300

    Create lesson98.example.js

---
+++ lesson98.example.js
@@ -0,0 +1,30 @@
+/*
+as we have see in earlier examples. objects can contain both
+nested objects and nested arrays. Similar to acessing nested object, array bracket 
+notation can be chained to acess nested arrays.
+here is an example of how to acess a nested array:
+
+*/
+
+var ourPets = [ 
+  {
+    animalType: "cat",
+    names: [
+      "Meowzer"
+      "Fluffly",
+      "Kit_cat"
+      
+      ]
+  },
+  
+  {
+    ANIMALTYPE: "dog",
+    names: [
+      "Spot",
+      "Bowser",
+      "Frankie"'
+      ]
+  }
+  ],
+    ourPets[0].names[1]
+ourPets[1];.names[0];
