b9ecc9f1bb5df21f4f970628b401a5136e027a0f
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 15:35:58 -0300

    Create lesson93.example.js

---
+++ lesson93.example.js
@@ -0,0 +1,28 @@
+// We can also delete properties from objects like this:
+
+delete ourDog.bark;
+
+
+//Example:
+
+var ourDog = {
+  "name":"Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends" ; ["everthing!"],
+  "bark": bow=wow"
+
+};
+
+delte ourDog.bark;
+
+{
+  
+  "name" : "Camper"
+  "legs": 4
+  "tails": 1;
+  "friends": ["everthing!"]
+  
+}
+
+//Delete the tais property from myDog
