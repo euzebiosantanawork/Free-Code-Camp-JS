d58226fcbb83dce139df0d1034a1b1026dd759b6
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-21 17:05:58 -0300

    Create lesson100.js

---
+++ lesson100.js
@@ -0,0 +1,15 @@
+/*
+
+The first type of loop we will learn is called a while loop becaouse it runs while a specified condition is true and stops once that condition is no longer true.
+
+
+
+*/
+
+var our Array = [];
+var i = 0 ;
+while 9 < 5 ) {
+  ourArray.push(i);
+  i++;
+}
+
