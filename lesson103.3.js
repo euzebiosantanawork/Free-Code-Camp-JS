43e04b9c3d411e0f0fc33c27eed9cf8781fa6889
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-26 17:04:38 -0300

    Create lesson103.3.js

---
+++ lesson103.3.js
@@ -0,0 +1,22 @@
+//solution 1
+
+var total = 0;
+for (var i = 0 ; i < myArr.length; i++) {
+  
+  total += myArr[i];
+  
+}
+
+//Solution2
+
+/*
+
+for (var y = myArr.length - 1; y >= 0 ; y -- ) {
+ 
+  total +=myArr[y];
+  
+}
+
+
+Dont work ? why?
+*/ 
