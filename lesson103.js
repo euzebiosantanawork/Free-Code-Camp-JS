e0b3c9cfa1cb4ab9d83219eb8db17aab0f00414e
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-26 15:01:07 -0300

    Create lesson103.js

---
+++ lesson103.js
@@ -0,0 +1,18 @@
+function phoneticLookup9val){
+  
+  var result = "";
+  var lookup = {
+    "alpha":"Adams",
+    "bravo":"Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo" : "Easy",
+    'foxtrot' : "Frank"
+  } ;
+  
+  //After converting our case statements into object properties
+  
+  result = lookup [val];
+  //Only change code above this line
+  return result;
+}
