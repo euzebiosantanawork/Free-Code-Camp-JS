ea57d053a233c0b8e9e981e397fa0b65d17c32a1
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-28 15:57:40 -0300

    Create lesson114.0.js

---
+++ lesson114.0.js
@@ -0,0 +1,9 @@
+/*Instead of generating a random whole number between zero and a given number like we did before, we can generate a random whole number that falls within a range of two specific numbers.
+
+To do this, we'll define a minimum number min and a maximum number max.
+
+Here's the formula we'll use. Take a moment to read it and try to understand what this code is doing:
+*/
+
+Math.floor(Math.radom()*(maz-min+1)) + min
+
