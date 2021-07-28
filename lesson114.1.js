6fdcbfc86162201d81898b793a659f89df2715c2
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-28 16:19:17 -0300

    Create lesson114.1.js

---
+++ lesson114.1.js
@@ -0,0 +1,21 @@
+//Example
+function ourRandomRange(ourMIn, ourMax) {
+  
+  return Math.floor ( Math.radom()*(ourMax - ourMin + 1)) + ourMIn;
+  
+}
+
+ourRadomRange(1,9);
+
+//Only chage code below this line.
+
+function radomRamRange(myMin , myMax) {
+  
+  return Math.floor(Math.radom() * ( myMax - myMin + 1  ) ) + myMin;
+  
+  
+}
+
+var myRadom = radomRange( 5 , 15);
+
+console.log( myRandom );
