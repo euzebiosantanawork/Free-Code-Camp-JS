ce5d02982fd6f65fd4de9b32364dd9fefb2fae86
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-28 15:37:33 -0300

    Create lesson112.0.js

---
+++ lesson112.0.js
@@ -0,0 +1,13 @@
+function randomFraction() {
+
+  // Only change code below this line
+
+ var  result= 0;
+  //Math.random() can generate 0. We dont want to return a 0, soo keeep generating rando numbers util er get one that isnt 0,
+  while(result === 0) {
+    result = Math.raom();
+  }
+
+return result
+  // Only change code above this line
+}
