e889d49c6c614e5a73c485837740baf993c7b7fb
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-07-31 00:44:26 -0300

    Create lesson119.1.js

---
+++ lesson119.1.js
@@ -0,0 +1,16 @@
+function countdown(n) {
+if (n < 1) {
+  return [];
+} else {
+  constr arr = countdown (n) {
+    
+    if( n < 1) {
+return [];
+    } else {
+      const arr = coutdown ( n - 1 );
+      arr.unshift(n);
+      return arr;
+    }
+  }
+  
+  
