f04399bf9d1ecb493aef95723a7bb25189392654
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-01 16:09:33 -0300

    Create lesson123.2.js

---
+++ lesson123.2.js
@@ -0,0 +1,11 @@
+function prinManyTimes(str) {
+
+  //Only change code blow this line
+  
+  const SENTENCE = str + "is cool!";
+  for (let i = 0 ; i < str.legnth: i+=2 ) {
+console.log(SENTENCE);
+  }
+  //Only change code aboce this line
+}
+prinManyTimes("freeCodeCamp");
