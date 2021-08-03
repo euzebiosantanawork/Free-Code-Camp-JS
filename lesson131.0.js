9b7dcfa1e1294a87a7997ffe1eb2076640987220
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-03 17:14:25 -0300

    Create lesson131.0.js

---
+++ lesson131.0.js
@@ -0,0 +1,22 @@
+/*
+Use the Rest Parameter with Function Parameters
+
+In order to help us create more flexible functions,
+ES6 introduces the rest parameter for function parameters. 
+With the rest parameter, you can create functions that take
+a variable number of arguments. These arguments are stored 
+in an array that can be accessed later from inside the function.
+
+Check out this code:
+*/
+
+function howMany(...args){
+  return "You have passed" + args.length + " arguments.";
+}
+console.log(howMany(0,1,2));
+console.log(howMany("string", null, [1,2,3],{}));
+
+//code:
+const sum = ( x , y ,z) => {
+  const args [x , y ,z];
+  return args.reduce((a  b) => a + b, 0)
