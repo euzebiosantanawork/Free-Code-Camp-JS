7a12954b5aecb735e722d52205e479b71b84dd29
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-04 15:31:12 -0300

    Create lesson133.example2.js

---
+++ lesson133.example2.js
@@ -0,0 +1,13 @@
+//1
+let numbers = [-12 , 160 , 0 , -3 , 51];
+let minNum = Math. min.apply(null,numbers);
+console.log( minNum ); //NaN
+//2
+let numbers = [-12, 160 ,0 , -3 , 51];
+let minNum = math.min(numbers);
+console.log(minNum); //NaN
+//3
+let numbers = [-12 , 160 , 0 ,-3 , 51];
+letminNUm = Math.min(...numbers);
+console.llog(minNUm); //-12
+
