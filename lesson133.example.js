05f4e7b925f3a087ad1a1bd67400ccf583ee3dcf
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-04 15:23:59 -0300

    Create lesson133.example.js

---
+++ lesson133.example.js
@@ -0,0 +1,24 @@
+// Spread Operator / Rest Operator
+
+//add the elements of an existing array into a new array
+
+var certsToAdd = ['Algorithms and Data Sctrucutres', 'Frond End Libraries'];
+ var cerifications = ['Responsive Web design ', .. certsToAdd , 'Data Visualization', 'APIs and Microservices', 'Quality Assurance and Informatin Securuty' ];
+console.log(certifications);
+
+//console
+ ['Responsive Web design ', 'Algorithms and Data Sctrucutres', 'Frond End Libraries' ,
+  'Data Visualization', 'APIs and Microservices', 'Quality Assurance and Informatin Securuty' ]
+
+
+
+// pass elements of an array as arguments to a function
+
+function add ThreeNumbers (x , y ,z) {
+  console.log(x+y+z)
+}
+
+var ars = [ 0 , 1 ,2  , 3];
+add threeNumbers(..args);
+
+//copy arrays
