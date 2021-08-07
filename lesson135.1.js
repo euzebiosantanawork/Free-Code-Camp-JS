ec3bf7476405759245febc95cbeb5d6dd1b3d2ce
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-07 01:09:13 -0300

    Create lesson135.1.js

---
+++ lesson135.1.js
@@ -0,0 +1,15 @@
+var myConcat =  function (arr1 , arr2 ) {
+   return arr1.concat(arr2);
+};
+
+console.log(MyConcat([1,2] [3,, 4, 5]))
+
+//Solution
+
+const myConcat = (arr1 ,arr2 ) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+//test your code
+
+console.log(myConcat([1,2],[3 , 4, 5]));
