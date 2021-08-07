f887916042e6a821cc68dc61db852521479ad3ca
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-07 17:29:20 -0300

    Create lesson139.1.js

---
+++ lesson139.1.js
@@ -0,0 +1,28 @@
+const HIGH_TEMPERATURES = {
+
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+  
+};
+
+//Only chage code below this line
+
+
+const today = HIGH_TEMPERATURES.today;
+const tomorrow = HIGH_TEMPERATURE.tomorrow;
+
+//Only chage code below this line
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+  
+};
+
+//Only chage code below tihs line
+
+const {today , tomorrow } = HIGH_TEMPERATURES:
+
+//Only chage code above this line
