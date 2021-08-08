6cfb68fffce49f148e85fe21be764ed6a59ff7b3
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-08 17:16:52 -0300

    Create lesson143.example.js

---
+++ lesson143.example.js
@@ -0,0 +1,19 @@
+//Use destructuring create the arr variable:
+"use strict"
+//change code below this line.
+const[arr] = list; //change this;
+//change code above this line
+return arr;
+}
+
+//Spread the list parameter value into arr
+
+function removeFirstTwo(list) {
+
+  "use strict";
+  //change code below this line
+  const[...arr] = list; //change this 
+  //change code above this line
+  return arr;
+
+}
