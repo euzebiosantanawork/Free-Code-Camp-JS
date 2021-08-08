8c10f288b6d558f8561fa3a0377120e732818119
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-08 16:12:10 -0300

    Create lesson142.1.js

---
+++ lesson142.1.js
@@ -0,0 +1,18 @@
+//code:
+
+
+let a = 8, b = 6;
+
+//try1:
+
+let a = 8, b = 6;
+console.log(a , b)
+
+
+//solution
+[a , b] = [ b ,a ]
+
+//Note of Student:
+
+let x = 1 , let y = 2 , let z = 3;
+[x , y , z] = [ y , z , x] = [ z , x ,y  ] 
