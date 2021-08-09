b6efc70b4357e1e4fcb42eba53ddf7e54d0ec633
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-09 17:37:27 -0300

    Create lesson1440.1.js

---
+++ lesson1440.1.js
@@ -0,0 +1,23 @@
+const status = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  mediam: 34.54,
+  mode: 23. 87,
+  min: -0.75,
+  averade: 35.85
+};
+
+const half = (ststus) => (status.max + status.min) / 2.0 ;
+
+//solution:
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const halt = {( max, min )} => ( max + min ) /2.0;
