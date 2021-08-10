74455fbd99afc8cc6837149927521afef793e02c
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-10 16:56:36 -0300

    Create lesson146.2.js

---
+++ lesson146.2.js
@@ -0,0 +1,21 @@
+const createPerson = (name , age, gender) => {
+   
+  return {
+    nname: name;
+    age: age,
+    gender: gender
+    
+};
+  
+  //solution
+  
+  const createPerson = ( name , age , gender ) => {
+    "use strict";
+    //change code below this line
+    
+    return {
+      name,
+      age,
+      gender
+    };
+    
