934ef34c1df568ff1db430b74368b4e27d0097d1
euzebiosantana.github <63376480+euzebiosantanawork@users.noreply.github.com>
2021-08-15 17:35:59 -0300

    Create lesson147.2.js

---
+++ lesson147.2.js
@@ -0,0 +1,11 @@
+??ony change coe below this line
+
+cosnt bicycle = {
+  gear: 2,
+  setGear: function(newGear) {
+    this.gear = newGear
+  }
+};
+//oly change code abover this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
